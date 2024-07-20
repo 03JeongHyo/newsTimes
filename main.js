61dc13e0584198ebfbd54ad2de20049e283b42cb
Jeonghyo Park <wjdgy2669@naver.com>
2024-07-20 21:45:45 +0900

    third commit

--- main.js
+++ main.js
@@ -3,40 +3,50 @@ let newsList = [];
 const menu = document.querySelectorAll(".menu button");//메뉴 들고오기
 menu.forEach(menu=>menu.addEventListener("click",(event)=> getNewsByCategory(event)))//item을 읽어옴
 
+
+const getNews = async () => {
+    try {
+        const response = await fetch(url);
+        if (response.status !== 200) { // 상태 코드가 200이 아닌 경우
+            const errorData = await response.json();
+            showError(`Error: ${errorData.message}`);
+            return;
+        }
+        const data = await response.json(); // await는 async와 사용
+        newsList = data.articles;
+        render();
+    } catch (error) {
+        showError('error');
+    }
+};
+
+let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr$apiKey=&{API_KEY}`)
+
 const getLatestNews = async () =>{
-    const url = new URL(
+    url = new URL(
         `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr$apiKey=&{API_KEY}`
       );
-    const response = await fetch(url);
-    const data = await response.json(); // await는 async와 사용
-    newsList = data.articles;
-    render();
-
+      getNews();
 };
 
 const getNewsByKeyword=async()=>{
     const keyword = document.getElementById("searchInput").value;
-    console.log("keyword",keyword);
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log("keyword data", data);
-    newsList = data.articles;
-    render();
+    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`)
+    getNews();
 };
 
 const getNewsByCategory= async(event)=>{
     const category = event.target.textContent.toLowerCase();
-    console.log("category",category);
-    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
-    
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("d",data);
-    newsList = data.articles;
-    render();
+    url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+    getNews();
 };
+
 const render=()=>{
+    if (newsList.length === 0) {
+        document.getElementById("news-board").innerHTML = '<p>No matches for your search</p>';
+        return;
+    }
+
     const newsHTML = newsList.map((news)=>
         `<div class="row news">
             <div class="col-lg-4">
@@ -57,6 +67,7 @@ const render=()=>{
     console.log("html", newsHTML);
     document.getElementById("news-board").innerHTML =newsHTML //어디에다 뉴스를 붙일지, 여러개 뉴스 만들기
 }
+
 
 
 
